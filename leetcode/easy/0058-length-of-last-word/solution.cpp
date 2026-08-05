0dd4673587399e739844da172d3bf4bd3b4ab75f
ratan-02 <kumarratan2006@gmail.com>
2026-08-06 04:11:59 +0530

    ✅ Length of Last Word | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 9 MB | 2026-08-05

--- leetcode/easy/0058-length-of-last-word/solution.cpp
+++ leetcode/easy/0058-length-of-last-word/solution.cpp
@@ -3,14 +3,12 @@ public:
     int lengthOfLastWord(string s) {
         int i = s.size() - 1;
         
-        // Skip trailing spaces
         while (i >= 0 && s[i] == ' ') {
             i--;
         }
         
         int len = 0;
-        
-        // Count characters of the last word
+       
         while (i >= 0 && s[i] != ' ') {
             len++;
             i--;
